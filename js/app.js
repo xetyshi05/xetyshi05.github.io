8e58c2f35447c5adc42ec1a8911448aa2b44b0b1
xetyshi <matmordasov@gmail.com>
2025-06-08 00:51:18 +0300

    m2 and bugs fix

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", () => {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  const preloader = document.getElementById("preloader")
-  preloader.style.opacity = "0"
-  preloader.style.visibility = "hidden"
-  setTimeout(() => {
-    preloader.remove()
-  }, 500)
-  const header = document.querySelector(".header")
-  setTimeout(() => {
-    header.classList.add("visible")
-  }, 200)
+	const vh = window.innerHeight * 0.01
+	document.documentElement.style.setProperty("--vh", `${vh}px`)
+	const preloader = document.getElementById("preloader")
+	preloader.style.opacity = "0"
+	preloader.style.visibility = "hidden"
+	setTimeout(() => {
+		preloader.remove()
+	}, 500)
+	const header = document.querySelector(".header")
+	setTimeout(() => {
+		header.classList.add("visible")
+	}, 200)
 })
