8a28e49ceb4acf21635029bf09a280c3e26e8449
xetyshi <matmordasov@gmail.com>
2025-06-09 00:52:58 +0300

    nav fix

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 window.addEventListener("load", () => {
 	// Высота окна пользователя
-	const vh = window.innerHeight * 0.01
-	document.documentElement.style.setProperty("--vh", `${vh}px`)
+	const vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty("--vh", `${vh}px`);
 	// Preloader
-	const preloader = document.getElementById("preloader")
-	preloader.style.opacity = "0"
-	preloader.style.visibility = "hidden"
+	const preloader = document.getElementById("preloader");
+	preloader.style.opacity = "0";
+	preloader.style.visibility = "hidden";
 	setTimeout(() => {
-		preloader.remove()
-	}, 500)
+		preloader.remove();
+	}, 500);
 	// Header animaton
-	const header = document.querySelector(".header")
+	const header = document.querySelector(".header");
 	setTimeout(() => {
-		header.classList.add("visible")
-	}, 200)
+		header.classList.add("visible");
+	}, 200);
 
 	// NAV
 
-	const nav_button = document.getElementById("navbutton")
-	const menu = document.getElementById("menu")
+	const nav_button = document.getElementById("navbutton");
+	const menu = document.getElementById("menu");
 
 	nav_button.addEventListener("click", () => {
-		const isOpen = menu.classList.toggle("menu-open")
-	})
+		const isOpen = menu.classList.toggle("menu-open");
+	});
 
 	document.addEventListener("click", (e) => {
 		if (
@@ -30,9 +30,17 @@ window.addEventListener("load", () => {
 			!menu.contains(e.target) &&
 			!nav_button.contains(e.target)
 		) {
-			menu.classList.remove("menu-open")
+			menu.classList.remove("menu-open");
 		}
-	})
+	});
+
+	document.querySelectorAll("#menu a").forEach((link) => {
+		link.addEventListener("click", () => {
+			if (menu.classList.contains("menu-open")) {
+				menu.classList.remove("menu-open");
+			}
+		});
+	});
 
 	// Анимация при скролле
 
@@ -40,35 +48,35 @@ window.addEventListener("load", () => {
 		(entries) => {
 			entries.forEach((entry) => {
 				if (entry.isIntersecting) {
-					entry.target.classList.add("scroll-visible")
-					observer1.unobserve(entry.target)
+					entry.target.classList.add("scroll-visible");
+					observer1.unobserve(entry.target);
 				}
-			})
+			});
 		},
 		{
 			threshold: 0.3,
 		}
-	)
+	);
 
 	document.querySelectorAll(".scroll-text").forEach((el) => {
-		observer1.observe(el)
-	})
+		observer1.observe(el);
+	});
 
 	const observer2 = new IntersectionObserver(
 		(entries) => {
 			entries.forEach((entry) => {
 				if (entry.isIntersecting) {
-					entry.target.classList.add("scroll-visible")
-					observer2.unobserve(entry.target)
+					entry.target.classList.add("scroll-visible");
+					observer2.unobserve(entry.target);
 				}
-			})
+			});
 		},
 		{
 			threshold: 1,
 		}
-	)
+	);
 
 	document.querySelectorAll(".scroll-img, .quote-block").forEach((el) => {
-		observer2.observe(el)
-	})
-})
+		observer2.observe(el);
+	});
+});
